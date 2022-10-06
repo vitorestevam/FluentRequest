8e12d4ecca45c0cc2c0c191c69a4b195429033cb
ArmoryNode <andy@armorynode.net>
2022-10-05 20:18:58 -0700

    Removed `createRequest` and moved contents into `t.Run` method, and moved `Test` and `ResponseResult` type definitions into function scope

--- fluentrequest_test.go
+++ fluentrequest_test.go
@@ -1,44 +1,53 @@
 package fluentrequest
 
 import (
-	"bytes"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type Test struct {
-	name   string
-	method string
-	url    string
-	body   string
-	want   ResponseResult
-}
-
-type ResponseResult struct {
-	responseBody string
-	statusCode   int
-}
-
 func TestRequest(t *testing.T) {
-	tests := []Test{
+	tests := []struct {
+		name   string
+		method string
+		url    string
+		header http.Header
+		body   string
+		want   struct {
+			responseBody string
+			statusCode   int
+		}
+	}{
 		{
 			name:   "Test GET request",
 			method: http.MethodGet,
 			url:    "https://jsonplaceholder.typicode.com/todos/1",
-			want: ResponseResult{
-				statusCode:   http.StatusOK,
+			header: http.Header{
+				"Content-Type": {"application/json; charset=UTF-8"},
+			},
+			want: struct {
+				responseBody string
+				statusCode   int
+			}{
 				responseBody: `{ "id": 1, "userId": 1, "title": "delectus aut autem", "completed": false }`,
+				statusCode:   http.StatusOK,
 			},
 		},
 		{
 			name:   "Test POST request",
 			method: http.MethodPost,
 			url:    "https://jsonplaceholder.typicode.com/todos/",
-			body:   `{ "id": 201, "userId": 2, "title": "foo", "body": "bar", "completed": true }`,
-			want: ResponseResult{
+			header: http.Header{
+				"Content-Type": {"application/json; charset=UTF-8"},
+			},
+			body: `{ "id": 201, "userId": 2, "title": "foo", "body": "bar", "completed": true }`,
+			want: struct {
+				responseBody string
+				statusCode   int
+			}{
 				statusCode:   http.StatusCreated,
 				responseBody: `{ "id": 201, "userId": 2, "title": "foo", "body": "bar", "completed": true }`,
 			},
@@ -47,8 +56,14 @@ func TestRequest(t *testing.T) {
 			name:   "Test PUT request",
 			method: http.MethodPut,
 			url:    "https://jsonplaceholder.typicode.com/todos/1",
-			body:   `{ "id": 1, "userId": 1, "title": "foo", "body": "bar", "completed": false }`,
-			want: ResponseResult{
+			header: http.Header{
+				"Content-Type": {"application/json; charset=UTF-8"},
+			},
+			body: `{ "id": 1, "userId": 1, "title": "foo", "body": "bar", "completed": false }`,
+			want: struct {
+				responseBody string
+				statusCode   int
+			}{
 				statusCode:   http.StatusOK,
 				responseBody: `{ "id": 1, "userId": 1, "title": "foo", "body": "bar", "completed": false }`,
 			},
@@ -57,8 +72,14 @@ func TestRequest(t *testing.T) {
 			name:   "Test PATCH request",
 			method: http.MethodPatch,
 			url:    "https://jsonplaceholder.typicode.com/todos/1",
-			body:   `{ "id": 1, "userId": 1, "title": "foobar", "body": "", "completed": true }`,
-			want: ResponseResult{
+			header: http.Header{
+				"Content-Type": {"application/json; charset=UTF-8"},
+			},
+			body: `{ "id": 1, "userId": 1, "title": "foobar", "body": "", "completed": true }`,
+			want: struct {
+				responseBody string
+				statusCode   int
+			}{
 				statusCode:   http.StatusOK,
 				responseBody: `{ "id": 1, "userId": 1, "title": "foobar", "body": "", "completed": true }`,
 			},
@@ -67,7 +88,13 @@ func TestRequest(t *testing.T) {
 			name:   "Test DELETE request",
 			method: http.MethodDelete,
 			url:    "https://jsonplaceholder.typicode.com/todos/1",
-			want: ResponseResult{
+			header: http.Header{
+				"Content-Type": {"application/json; charset=UTF-8"},
+			},
+			want: struct {
+				responseBody string
+				statusCode   int
+			}{
 				statusCode:   http.StatusOK,
 				responseBody: `{}`,
 			},
@@ -76,28 +103,18 @@ func TestRequest(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			createRequest(t, tt.method, tt.url, tt.body, tt.want)
+			resp, err := FluentRequest().
+				Method(tt.method).
+				Body(strings.NewReader(tt.body)).
+				Header(tt.header).
+				Url(tt.url).
+				Run()
+
+			responseBody, _ := io.ReadAll(resp.Body)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
+			assert.JSONEq(t, tt.want.responseBody, string(responseBody))
 		})
 	}
-}
-
-func createRequest(t *testing.T, method string, url string, requestBody string, want ResponseResult) {
-	bodyBytes := []byte(requestBody)
-
-	header := http.Header{
-		"Content-Type": {"application/json; charset=UTF-8"},
-	}
-
-	resp, err := FluentRequest().
-		Method(method).
-		Body(bytes.NewBuffer(bodyBytes)).
-		Header(header).
-		Url(url).
-		Run()
-
-	responseBody, _ := io.ReadAll(resp.Body)
-
-	assert.NoError(t, err)
-	assert.JSONEq(t, want.responseBody, string(responseBody))
-	assert.Equal(t, want.statusCode, resp.StatusCode)
 }
